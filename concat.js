5b04b323a8ea5d1d89ba9497a40016b93ef9ac1d
aschaves1976 <roots17@gmail.com>
2019-01-16 21:43:41 -0200

    Array - Concat

---
+++ concat.js
@@ -0,0 +1,9 @@
+const filhas =["Maria", "Joana"]
+const filhos = ["Lucas", "Ebiwyby"]
+const todos = filhas.concat(filhos, "Mais um filho")
+
+console.log(`Todos os filhos:  ${todos}
+As filhas:  ${filhas}
+Os Filhos:  ${filhos}`)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7], 8]))
